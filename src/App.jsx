b25ca1c020505435e30d64101dfab9900780df06
dhafnanbd <dhafnanbd@gmail.com>
2025-09-20 13:03:15 +0600

    React add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Countries from './Components/Countries/Countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is the beginning...</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>This is comming from countries component</h2>
+        </div>
+    );
+};
+
+export default Countries;
